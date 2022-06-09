562161e83f96b1c03052c5be4afdef7d5b1ae43e
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-09 11:35:24 -0400

    working 404 page done

--- Develop/routes/diagnostics.js
+++ Develop/routes/diagnostics.js
@@ -5,11 +5,13 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+
 });
 
 module.exports = diagnostics;
